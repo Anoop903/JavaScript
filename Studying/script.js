ae2c1db6d61c8b65efec9af4a34c06a82b85ed22
AnoopSuresh <anoop.suresh@beinex.com>
2020-10-10 10:59:33 +0530

    Apply, Bind, Call

--- Studying/script.js
+++ Studying/script.js
@@ -35,3 +35,62 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 4);
 })();
+
+//Bind, Call and Apply
+
+maqbool = {
+  name: "Maqbool",
+  job: "Developer",
+  age: 28,
+  presentation: function (style, time) {
+    if (style === "Formal") {
+      console.log(
+        "Good " +
+          time +
+          ". I'm a " +
+          this.job +
+          ". My name is " +
+          this.name +
+          ". I'm " +
+          this.age +
+          "year's of old"
+      );
+    }
+    if (style === "Casual") {
+      console.log(
+        "What's up!!!!" +
+          " I'm " +
+          this.name +
+          ". I'm a " +
+          this.job +
+          ". I'm just " +
+          this.age +
+          " years's of old. Have a nice " +
+          time
+      );
+    }
+  },
+};
+
+nelsy = {
+  name: "Nelsy",
+  job: "Full Stack Developer",
+  age: 25,
+};
+maqbool.presentation("Formal", "Morning");
+
+//Good Morning. I'm a Developer. My name is Maqbool. I'm 28year's of old
+
+maqbool.presentation.call(nelsy, "Casual", "Morning");
+
+//  What's up!!!! I'm Nelsy. I'm a Full Stack Developer. I'm just 25 years's of old. Have a nice Morning
+
+// Apply method only accept two arguments... One is this key word, and other is all other params as an array
+maqbool.presentation.apply(nelsy, ["Formal", "Morning"]);
+
+// Good Morning. I'm a Full Stack Developer. My name is Nelsy. I'm 25year's of old
+
+//Bind Function Create a copy of this Function
+var maqboolFriendly = maqbool.presentation.bind(maqbool, "Casual");
+
+maqboolFriendly("Morning");
